38618601ceff333523d5518f50edea7c8094bdcc
Nils Martel <nils.martel@graphmasters.net>
2019-12-27 13:17:35 +0100

    Getting started

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::error::Error;
+
 fn main() {
-    println!("Hello, world!");
+    let username = std::env::args().nth(1).expect("specify username");
+}
+
+fn get_repos(username: &str) -> Result<Vec<String>, Box<Error>> {
+    use std::collections::HashMap;
+    let resp = reqwest::get(&format!("https://api.github.com/users/{}/repos", username))?;
+
+    dbg!(resp);
+    Ok(vec![])
 }
