c8ff37b0eb8698cec2e70f84523f057bf72c4c0f
Nils Martel <nils.martel@graphmasters.net>
2019-12-27 16:02:57 +0100

    Parsing Headers

--- src/repo.rs
+++ src/repo.rs
@@ -1,6 +1,8 @@
 use serde_json::Value;
 use std::error::Error;
 
+///
+/// Print all of the users Repositories to the stdout
 pub fn print_repos(user: &str) {
     match get_repos(user) {
         Err(e) => eprintln!("{}", e),
@@ -13,8 +15,9 @@ pub fn print_repos(user: &str) {
 }
 
 fn get_repos(username: &str) -> Result<Vec<String>, Box<dyn Error>> {
-    let resp: Value =
-        reqwest::get(&format!("https://api.github.com/users/{}/repos", username))?.json()?;
+    let mut resp = reqwest::get(&format!("https://api.github.com/users/{}/repos", username))?;
+
+    let resp: Value = resp.json()?;
 
     let res: Vec<String> = match resp {
         Value::Array(a) => a
@@ -31,4 +34,65 @@ fn get_repos(username: &str) -> Result<Vec<String>, Box<dyn Error>> {
     };
 
     Ok(res)
+}
+
+fn collect_repos(url: &str) -> Result<Vec<String>, Box<dyn Error>> {
+    let mut resp = reqwest::get(url)?;
+    let link = resp.headers().get("link").unwrap().to_str().unwrap();
+    let (_, (next, last)) = parse_link(&link).unwrap();
+
+    let resp: Value = resp.json()?;
+
+    let res: Vec<String> = match resp {
+        Value::Array(a) => a
+            .into_iter()
+            .map(|i| match i {
+                Value::Object(o) => match &o["name"] {
+                    Value::String(s) => s.to_owned(),
+                    _ => unreachable!(),
+                },
+                _ => unreachable!(),
+            })
+            .collect(),
+        _ => unreachable!(),
+    };
+
+    Ok(res)
+}
+
+fn parse_link(input: &str) -> nom::IResult<&str, (&str, &str)> {
+    // Sample input
+    // let x = "<https://api.github.com/user/28377948/repos?page=2>; rel=\"next\", <https://api.github.com/user/28377948/repos?page=2>; rel=\"last\"
+    use nom::{
+        bytes::complete::{tag, take_until},
+        character::complete::char,
+        sequence::{preceded, tuple},
+    };
+
+    tuple((
+        preceded(char('<'), take_until(">; rel=\"next\", ")),
+        preceded(
+            tag(">; rel=\"next\", "),
+            preceded(char('<'), take_until(">; rel=\"last\"")),
+        ),
+    ))(input)
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn parse_link() {
+        let input = "<https://api.github.com/user/12345678/repos?page=2>; rel=\"next\", <https://api.github.com/user/12345678/repos?page=2>; rel=\"last\"";
+
+        let (rest, result) = super::parse_link(input).unwrap();
+
+        assert_eq!(
+            "https://api.github.com/user/12345678/repos?page=2",
+            result.0
+        );
+        assert_eq!(
+            "https://api.github.com/user/12345678/repos?page=2",
+            result.1
+        );
+    }
 }
