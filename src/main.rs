5655fec301af9b29af1d7be2d53e75346f3ea0dd
Nils Martel <nils.martel@graphmasters.net>
2019-12-27 14:58:43 +0100

    Rename Subcommand Repos to Repo

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
-mod repos;
+mod repo;
 use structopt::StructOpt;
 
 fn main() {
     let args = Arguments::from_args();
     match args {
-        Arguments::Repos { user } => repos::print_repos(&user),
+        Arguments::Repo { user } => repo::print_repos(&user),
     };
 }
 
 #[derive(StructOpt, Debug)]
 #[structopt(about = "easy interface into the GitHub API")]
 enum Arguments {
-    Repos {
+    Repo {
         #[structopt()]
         user: String,
     },
